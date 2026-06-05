b75565c6e1f20bf6452169b99085091a5e019f58
AlexXAwada <alexandreawada@hotmail.com>
2026-06-05 07:34:59 -0300

    [MIG] l10n_py: convert chart template to Odoo 19 format
    - Removed old XML-based chart template (account.chart.template is AbstractModel)
    - Added template_py.py with @template('py') decorator
    - Moved account/account.group/tax.group to data/template/{model}-py.csv
    - Converted tax templates to data/template/account.tax-py.csv
    - Converted fiscal positions to data/template/account.fiscal.position-py.csv
    - 222 accounts, 73 account groups, 6 taxes, 9 fiscal positions

--- l10n_py/__manifest__.py
+++ l10n_py/__manifest__.py
@@ -10,16 +10,6 @@
         "account",
     ],
     "data": [
-        # Data - orden importante: try_loading debe ser el ÚLTIMO
-        # porque necesita que todos los templates existan antes de ejecutarse
-        "data/account_tax_group_data.xml",
-        "data/account_chart_template_data.xml",
-        "data/account.account.template.csv",
-        "data/account_group.xml",
-        "data/account_chart_template_account_account_link.xml",
-        "data/account_tax_template_data.xml",
-        "data/fiscal_position_template_data.xml",
-        "data/account_chart_template_configure_data.xml",
         # Views
     ],
     "demo": [],
