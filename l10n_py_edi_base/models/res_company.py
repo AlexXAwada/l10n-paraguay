46c07aceb93cff78ab90c2835bd77bfbbff76067
AlexXAwada <alexandreawada@hotmail.com>
2026-06-13 14:09:48 -0300

    Fix W8150: use relative import instead of absolute import

--- l10n_py_edi_base/models/res_company.py
+++ l10n_py_edi_base/models/res_company.py
@@ -2,7 +2,7 @@
 
 from odoo import api, fields, models
 
-from odoo.addons.l10n_py_base.validators.ruc_validator import RUCValidator
+from ..l10n_py_base.validators.ruc_validator import RUCValidator
 
 
 class ResCompany(models.Model):
